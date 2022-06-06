1cd0bc11879f95d1b1acfee41888b79119ae808d
Abhinav Kumar <abhinavkumar045@gmail.com>
2022-06-07 04:34:35 +0530

    Phase-2 training of Transfer Learning on 2-Way subset RF START data with: (d) No face
    Pre-trained model used: TL model trained on 2-Way subset RF UK data

--- code/mainClassifier.py
+++ code/mainClassifier.py
@@ -34,7 +34,7 @@ def str2bool(v):
 
 
 # NOTE - check CHECKPOINTS_PATH before running
-CHECKPOINTS_PATH = 'saved_models/2WayGazeClassification/START-2Way/RF_subsetSTART-2Way-train-10percent_TL-Phase1-2WayUK-blackedFace_checkpoints_train'
+CHECKPOINTS_PATH = 'saved_models/2WayGazeClassification/START-2Way/RF_subsetSTART-2Way-train-10percent_TL-Phase1-2WayUK-noFace_checkpoints_train'
 CHECKPOINT_LOAD_FILE = 'checkpoint_train_35.pth.tar'
 CHECKPOINT_SAVE_FILE = 'checkpoint'
 METAFILE = 'metadata_subset_train-10.0percent.mat'
@@ -167,16 +167,16 @@ def main():
 			print('Loading checkpoint of epoch %05d with best_prec1 %.5f (which is the mean L2 (i.e. linear) error in cm)...' % (saved['epoch'], saved['best_prec1']))
 			state = saved['state_dict']
 
-			# # Removing 'eyeModel' & 'eyesFC' from the pre-trained model (trained with 'eyeModel' & 'eyesFC' in Phase-1 of TL) to be loaded for Phase-2 of TL without eyes input
-			# modulesToRemove = []
-			# for k in state:
-			# 	if ('eyeModel' in k) or ('eyesFC' in k):
-			# 		modulesToRemove.append(k)
-			# for moduleToRemove in modulesToRemove:
-			# 	del state[moduleToRemove]
-			# # Since face input is removed from NN, no. of input neurons in linear layer 'fc.0' changes from 320 to 256. So, 'fc.0.weight' needs to be of shape (128,256), instead of (128,320) saved in the model to be loaded.
-			# # So, temporarily loading dummy tensor of zeros of shape (128,256) in state['fc.0.weight']. Later, model.fc is initialized with new nn.Sequential(), so dummy tensor removed.
-			# state['fc.0.weight'] = torch.zeros((128, 192), device=GPU_device)
+			# Removing 'eyeModel' & 'eyesFC' from the pre-trained model (trained with 'eyeModel' & 'eyesFC' in Phase-1 of TL) to be loaded for Phase-2 of TL without eyes input
+			modulesToRemove = []
+			for k in state:
+				if ('faceModel' in k):
+					modulesToRemove.append(k)
+			for moduleToRemove in modulesToRemove:
+				del state[moduleToRemove]
+			# Since face input is removed from NN, no. of input neurons in linear layer 'fc.0' changes from 320 to 256. So, 'fc.0.weight' needs to be of shape (128,256), instead of (128,320) saved in the model to be loaded.
+			# So, temporarily loading dummy tensor of zeros of shape (128,256) in state['fc.0.weight']. Later, model.fc is initialized with new nn.Sequential(), so dummy tensor removed.
+			state['fc.0.weight'] = torch.zeros((128, 256), device=GPU_device)
 
 			try:
 				model.module.load_state_dict(state)
@@ -191,7 +191,7 @@ def main():
 			# Required only if loading a regression model to be used for Transfer Learning for classification #
 			#############################
 			# Fix all conv layers
-			model.faceModel.conv.requires_grad = False
+			# model.faceModel.conv.requires_grad = False
 			model.eyeModel.requires_grad = False
 
 			# Reset (i.e. trainable & initialized with random weights) last 2 FC layers with o/p of last FC layer as class labels L/R
@@ -253,7 +253,7 @@ def main():
 	# criterion = classifAccuracy
 
 	##### Specify the parameters to be optimized (i.e. only the trainable params) in Transfer Learning #####
-	trainableParams = list(model.faceModel.fc.parameters()) + list(model.eyesFC[0].parameters()) + list(model.gridModel.parameters()) + list(model.fc.parameters())
+	trainableParams = list(model.eyesFC[0].parameters()) + list(model.gridModel.parameters()) + list(model.fc.parameters())
 	optimizer = torch.optim.SGD(trainableParams,
 								base_lr, momentum=momentum,
 								weight_decay=weight_decay)
